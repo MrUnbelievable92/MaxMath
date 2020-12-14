7c8a04febdcf65e40686c7655d0651416c787271
MrUnbelievable92 <mkalimon@gmx.de>
2020-12-14 21:12:15 +0100

    Release as preview Unity Package

--- Runtime/AssemblyInfo.cs
+++ Runtime/AssemblyInfo.cs
@@ -29,8 +29,8 @@ using System.Diagnostics.CodeAnalysis;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.8.0.6")]
-[assembly: AssemblyFileVersion("0.8.0.6")]
+[assembly: AssemblyVersion("0.8.6")]
+[assembly: AssemblyFileVersion("0.8.6")]
 [assembly: AssemblyInformationalVersion("0.8 Pre-Release")]
 
 [assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Unity.Mathematics API consistency")]
