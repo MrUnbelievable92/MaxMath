92c98782b0df3d9e93028a61b42778065fe39f24
MrUnbelievable92 <mkalimon@gmx.de>
2020-12-21 01:16:48 +0100

    No

--- Runtime/Functions/Comparison/CompareTo.cs
+++ Runtime/Functions/Comparison/CompareTo.cs
@@ -33,5 +33,20 @@ namespace MaxMath
         {
             return touint8(x > y) - touint8(x < y);
         }
+
+
+        /// <summary>       Returns -1 if x is smaller than y, 1 if x is greater than y or 0 if both are equal.       </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]   [return: AssumeRange(-1, 1)]
+        public static int compareto(float x, float y)
+        {
+            return touint8(x > y) - touint8(x < y);
+        }
+
+        /// <summary>       Returns -1 if x is smaller than y, 1 if x is greater than y or 0 if both are equal.       </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]   [return: AssumeRange(-1, 1)]
+        public static int compareto(double x, double y)
+        {
+            return touint8(x > y) - touint8(x < y);
+        }
     }
 }
